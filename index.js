63740bc221131565344078060b4a284ea92ddfc1
saifulislammunna <saifulmunna28@gmail.com>
2021-10-27 00:35:19 +0600

    update

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ Every project
 6.  heroku login
 7.  heroku create (only one time for a project)
 8.  command: git push heroku main
+9. heroku > app > settings > add var (add DB_USER, DB_PASS)
 
 -----------------
 
